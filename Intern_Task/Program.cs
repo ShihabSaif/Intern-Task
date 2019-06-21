d7369cc25d4f36c80710590de5fda9c6e4f371a3
ShihabSaif <a.b.m.shihab57@gmail.com>
2019-06-21 14:55:22 +0600

    first commit

--- Intern_Task/Program.cs
+++ Intern_Task/Program.cs
@@ -10,6 +10,27 @@ namespace Intern_Task
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write your input sentence : ");
+            string name = Console.ReadLine();
+            string[] tokens = name.Split(' ');
+            int diff = 0;
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(tokens[i]); //first word remains as usual
+                }
+                else
+                {
+                    diff += tokens[i - 1].Length; //calculating number of spaces needed to make the pattern
+                    for (int j = 0; j < diff; j++)
+                    {                      
+                        Console.Write(" "); //putting number of spaces before each word to form up the pattern
+                    }
+                    Console.WriteLine(tokens[i]);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
